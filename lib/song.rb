1e9d2a5832e5bd4cbc01d473dcfba9e7cfab295f
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-07-04 07:19:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,9 +16,9 @@ class Song
     song
   end
   
-  def self.new_by_name
+  def self.new_by_name(song_name)
     song = self.new
-    song.name = song.save
+    song.name = song_name
     song
   end
 end
