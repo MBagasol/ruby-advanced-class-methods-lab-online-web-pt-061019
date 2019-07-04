9e28e3177cc33a3fdf7430dd0419834796c82593
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-07-04 07:40:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,27 +42,12 @@ class Song
 
   def self.new_from_filename(filename)
     parts = filename.split(" - ")
-    artist_name = songs[0]
-    song_name = songs[1].gsub(".mp3", "")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
 
     song = self.new
     song.name = song_name
     song.artist_name = artist_name
     song
-  end
-
-  def self.create_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name = parts[0]
-    song_name = parts[1].gsub(".mp3", "")
-
-    song = self.create
-    song.name = song_name
-    song.artist_name = artist_name
-    song
-  end
-
-  def self.destroy_all
-    self.all.clear
   end
 end
