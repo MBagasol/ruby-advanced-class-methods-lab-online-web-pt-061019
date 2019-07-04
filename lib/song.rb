47369779da8c472cf8188f6778d20681c2a1abbe
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-07-04 07:26:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,8 +42,8 @@ class Song
 
   def self.new_from_filename(filename)
     songs = filename.split(" - ")
-    artist_name = parts[0]
-    song_name = parts[1].gsub(".mp3", "")
+    artist_name = songs[0]
+    song_name = songs[1].gsub(".mp3", "")
 
     song = self.new
     song.name = song_name
